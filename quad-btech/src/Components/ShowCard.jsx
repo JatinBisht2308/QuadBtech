2be6cb9036ea2396c9770c3bb1f687ccb81daf25
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-05-13 01:58:01 +0530

    :fire: completed the first page that is the all shows page

--- quad-btech/src/Components/ShowCard.jsx
+++ quad-btech/src/Components/ShowCard.jsx
@@ -3,17 +3,28 @@ import "../Styles/showcard.css";
 const ShowCard = (props) => {
   return (
     <div className="ShowCard">
-      <img
-        src={
-          props.showInfo.show.image
-            ? props.showInfo.show.image.medium
-            : "https://www.shutterstock.com/image-illustration/profile-picture-600w-404137684.jpg"
-        }
-        alt="image of the show"
-      />
-      <h1>{props.showInfo.show.name}</h1>
-      <h2>{props.showInfo.show.language}</h2>
-      <h2>{props.showInfo.show.weight}</h2>
+      <div className="card-top">
+        <img
+          src={
+            props.showInfo.show.image
+              ? props.showInfo.show.image.medium
+              : "https://www.shutterstock.com/image-illustration/profile-picture-600w-404137684.jpg"
+          }
+          alt="image of the show"
+        />
+      </div>
+      <div className="card-bottom">
+        <h3>{props.showInfo.show.name}</h3>
+        <p>{props.showInfo.show.language}</p>
+        <p>{props.showInfo.show.genres[0]}</p>
+        <p>
+          Rating&nbsp;&nbsp;
+          {props.showInfo.show.rating.average == null
+            ? "0"
+            : props.showInfo.show.rating.average}
+        </p>
+        <button className="card-btn">more</button>
+      </div>
     </div>
   );
 };
