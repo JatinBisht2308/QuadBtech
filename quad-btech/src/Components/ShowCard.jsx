87cde7c06e84c20e4a9eeb6d67656fdddbc2d2d1
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-05-13 17:04:52 +0530

    :fire: routing is done successfully

--- quad-btech/src/Components/ShowCard.jsx
+++ quad-btech/src/Components/ShowCard.jsx
@@ -4,7 +4,7 @@ import "../Styles/showcard.css";
 const ShowCard = (props) => {
   const navigate = useNavigate();
   const goToBookingPage = () =>{
-    navigate("/show/:showId",{state:props.showInfo.show});
+    navigate(`show/${props.showInfo.show.id}`,{state:props.showInfo.show});
   }
   return (
     <div className="ShowCard">
